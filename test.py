acd20f1ecf4e11638d1f5864a424d464ea52f889
allknowingfrog <allknowingfrog@gmail.com>
2015-04-03 17:58:45 -0500

    begin move to 2x2 grouping

--- test.py
+++ test.py
@@ -3,67 +3,52 @@ import csv
 
 teamsCount = 2
 
+def group(source):
+    destination = {}
+    outliers = {}
+    for s in strengths:
+        outliers[s] = {
+            'high': {
+                'group': '',
+                'score' : 0
+            },
+            'low': {
+                'group': '',
+                'score' : 10
+            }
+        }
+
+    for g in source:
+        group = source[g]
+        for p in group:
+            people = group[p]
+            for s in people:
+                score = people[s]
+                if score > outliers[s]['high']['score']:
+                    outliers[s]['high']['score'] = score
+                    outliers[s]['high']['group'] = g
+                elif score < outliers[s]['low']['score']:
+                    outliers[s]['low']['score'] = score
+                    outliers[s]['low']['group'] = g
+
+    for s in outliers:
+        outliers[s]['spread'] = outliers[s]['high']['score'] - outliers[s]['low']['score']
+
 people = {}
 with open('strengths.csv') as csvfile:
     reader = csv.DictReader(csvfile)
+    strengths = reader.fieldnames[1:]
     for row in reader:
         people[row['Name']] = {}
-        people[row['Name']]['a'] = int(row['A'])
-        people[row['Name']]['b'] = int(row['B'])
-        people[row['Name']]['c'] = int(row['C'])
+        for s in strengths:
+            people[row['Name']][s] = int(row[s])
 
-outliers = {
-    'a': {
-        'high': {
-            'person': '',
-            'score' : 0
-        },
-        'low': {
-            'person': '',
-            'score' : 10
-        }
-    },
-    'b': {
-        'high': {
-            'person': '',
-            'score' : 0
-        },
-        'low': {
-            'person': '',
-            'score' : 10
-        }
-    },
-    'c': {
-        'high': {
-            'person': '',
-            'score' : 0
-        },
-        'low': {
-            'person': '',
-            'score' : 10
-        }
-    }
-}
-for p in people:
-    for s in people[p]:
-        score = people[p][s]
-        if score > outliers[s]['high']['score']:
-            outliers[s]['high']['score'] = score
-            outliers[s]['high']['person'] = p
-        elif score < outliers[s]['low']['score']:
-            outliers[s]['low']['score'] = score
-            outliers[s]['low']['person'] = p
-
-for s in outliers:
-    print(
-        s, 
-        outliers[s]['high']['person'], 
-        outliers[s]['high']['score'], 
-        outliers[s]['low']['person'], 
-        outliers[s]['low']['score']
-    )
-
-teams = []
-for i in range(0, teamsCount):
-    teams.append([])
-
+groups = []
+for g in people:
+    person = people[g]
+    group = {}
+    group['people'][g] = person
+    group['strengths'] = {}
+    s in person:
+        group['strengths'][s] = person[s]
+    groups.append(group)
